a0e3b5fa4f59eee320df7be24dfe57f86d86b001
Jason Walton <jwalton@benbria.ca>
2020-08-03 22:24:00 -0400

    fix: Automate setting shell to zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+chsh -s /bin/zsh
 if [ ! -e ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
