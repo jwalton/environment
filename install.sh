11ea0a31a02f52a9146987ce90a12bab1beaeac9
Jason Walton <jwalton@benbria.ca>
2020-08-05 09:42:33 -0400

    feat: Install nvm.

--- install.sh
+++ install.sh
@@ -11,4 +11,7 @@ echo Copying files...
 cp -r ./home/ ~/
 cp ./home/.config/zsh/jwalton.zsh-theme ~/.oh-my-zsh/custom/themes
 
+echo Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
 echo Done!
