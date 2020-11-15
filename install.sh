b1c39b791af65175a921224cc616b66ae11ecc5a
Jason Walton <jwalton@solinkcorp.com>
2020-11-15 13:01:41 -0500

    fix: Minor fixes for setting up zsh.

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 echo Setting up zsh...
 if ! which zsh; then
-    if ! which apt; then
-        echo "Can't install zsh!"
-    else
+    if which apt; then
         sudo apt install zsh -y
+    else
+        echo "** Can't install zsh!"
     fi
 fi
 
-if which zsh; then
+ZSH=$(which zsh)
+if [ -n "${ZSH}" ] && [ "${ZSH}" != "${SHELL}" ]; then
+    echo "Setting default shell to zsh"
     chsh -s $(which zsh)
-    if [ ! -e ~/.oh-my-zsh ]; then
-        echo "Installing oh-my-zsh"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    fi
+fi
+if [ ! -e ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 ./scripts/install-homedir.sh
