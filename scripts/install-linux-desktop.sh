71ea48e81c8d9035dbd6499763dcb2690accfc44
Jason Walton <jwalton@solinkcorp.com>
2022-02-28 11:26:08 -0500

    fix(linux): Configure focus follows mouse.

--- scripts/install-linux-desktop.sh
+++ scripts/install-linux-desktop.sh
@@ -14,3 +14,16 @@ if dpkg -s xfce4-session &> /dev/null; then
     echo     Window Manager => Style
     echo
 fi
+
+# XFCE settings
+if which xfconf-query > /dev/null; then
+    # Focus follows mouse
+    xfconf-query -c xfwm4 -p /general/click_to_focus -s false
+    xfconf-query -c xfwm4 -p /general/focus_delay -s 0
+fi
+
+# Gnome settings
+if which gsettings > /dev/null; then
+    # Focus follows mouse
+    gsettings set org.gnome.desktop.wm.preferences focus-mode mouse
+fi
