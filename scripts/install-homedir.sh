8841cb953a36cb15cc9aa46388f09a13ad41dbfe
Jason Walton <jwalton@solinkcorp.com>
2024-02-29 16:04:58 -0500

    feat: Add --dry-run to scripts/install-homedir.sh

--- scripts/install-homedir.sh
+++ scripts/install-homedir.sh
@@ -1,41 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
+# Run with `--dry-run` for dry run.
+RSYNC_FLAGS=""
+DRY_RUN="FALSE"
+if [ -n "$1" ]; then
+    echo "Dry run mode."
+    RSYNC_FLAGS="--dry-run"
+    DRY_RUN="TRUE"
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/common.sh"
 
 # Backup git singing key
+log "Backup signing key..."
 GIT_SIGNING_KEY=$(git config --global user.signingkey || echo "")
 
 log "Copying files..."
-cp -r "${DIR}/../home/." "${HOME}/"
-cp "${DIR}/../oh-my-zsh-themes/"* "${HOME}/.oh-my-zsh/custom/themes"
+rsync -av ${RSYNC_FLAGS} "${DIR}/../home/." "${HOME}/"
+rsync -av ${RSYNC_FLAGS} "${DIR}/../oh-my-zsh-themes/"* "${HOME}/.oh-my-zsh/custom/themes"
 
-# Restore singing key settings
-if [ -z "${GIT_SIGNING_KEY}" ]; then
-    git config --global --unset user.signingkey
-else
-    git config --global user.signingkey "${GIT_SIGNING_KEY}"
+# Restore signing key settings
+if [ "${DRY_RUN}" != "TRUE" ]; then
+    log "Restore signing key..."
+    if [ -z "${GIT_SIGNING_KEY}" ]; then
+        git config --global --unset user.signingkey
+    else
+        git config --global user.signingkey "${GIT_SIGNING_KEY}"
+    fi
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     log "Copying Mac files..."
-    cp -r "${DIR}/../machome/." "${HOME}/"
-
-    if which pinentry-mac > /dev/null && which gpgconf > /dev/null; then
-        log "Generating gpg-agent.conf"
-        mkdir -p ~/.gnupg
-        echo "
-# This file was automatically generated.
-# After editing this file, run `gpgconf --kill gpg-agent`
-
-# 8 Hours
-max-cache-ttl 28800
-default-cache-ttl 28800
-pinentry-program $(which pinentry-mac)
-" > ~/.gnupg/gpg-agent.conf
-        gpgconf --kill gpg-agent
-    else
-        log "Skipping gpg-agent.conf"
-    fi
+    rsync -av ${FLAGS} "${DIR}/../machome/." "${HOME}/"
 fi
