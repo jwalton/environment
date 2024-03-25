2a6806dd7434619cde562d7391d4cc72107fcf7d
Jason Walton <jwalton@solinkcorp.com>
2024-03-25 16:31:50 -0400

    fix: Update rust-coverage script.

--- home/bin/rust-coverage.sh
+++ home/bin/rust-coverage.sh
@@ -2,23 +2,29 @@
 
 set -e
 
-# By Oguz Bilgener
-#
-# A script to run unit tests and generate coverage locally.
-#
-# Prerequisites:
-#
-# $ rustup install nightly
-# $ cargo install grcov
+GIT_ROOT=$(git rev-parse --show-toplevel)
+COVERAGE_DIR="${GIT_ROOT}/target/coverage/html"
 
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
-export RUSTDOCFLAGS="-Cpanic=abort"
+if ! which grcov > /dev/null; then
+    echo "Installing grcov..."
+    cargo install grcov
+    rustup component add llvm-tools-preview
+fi
 
-cargo +nightly clean
-cargo +nightly build
-cargo +nightly test
+cargo clean
+CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test $*
+grcov . \
+    --binary-path "${GIT_ROOT}/target/debug/deps/" \
+    --source-dir . \
+    --output-types html \
+    --branch \
+    --ignore-not-existing \
+    -o "${COVERAGE_DIR}"
+find . -name "*.profraw" -print0 | xargs -0 rm
 
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
-
-open target/debug/coverage/index.html
+# If this is MacOS, open the coverage report.
+if which open > /dev/null; then
+    open "${GIT_ROOT}/target/coverage/html/index.html"
+else
+    echo "Coverage report available at: ${COVERAGE_DIR}/index.html"
+fi
